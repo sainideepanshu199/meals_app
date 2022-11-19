58840ad22dbd5502de449ffd79d1aaf3222ac133
Deepanshu Saini <sainids785@gmail.com>
2022-11-19 18:44:49 +0530

    added named route to navigator with arguments

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
+  final String id;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.title, this.color, this.id);
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(MaterialPageRoute(
-      builder: (_) {
-        return CategoryMealsScreen();
-      },
-    ));
+    Navigator.of(ctx)
+        .pushNamed('/category-Meals', arguments: {'id': id, 'title': title});
   }
 
 // constructor is added to assign values to these properties
