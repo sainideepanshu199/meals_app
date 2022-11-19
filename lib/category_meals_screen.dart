c36fb5780ae5b7d944822c5f03abc0e9045dca7a
Deepanshu Saini <sainids785@gmail.com>
2022-11-19 18:48:04 +0530

    added route arguments to file and used them to display category title on appbar

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,10 +1,20 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryTitle, this.categoryId);
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
     return Scaffold(
-      appBar: AppBar(title: Text('The Recipies')),
+      appBar: AppBar(title: Text(categoryTitle)),
       body: Center(
         child: Text('The recipe for the category'),
       ),
