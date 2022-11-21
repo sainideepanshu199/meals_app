cbcdcf6963152c8669e1cd1359d3cd1786326b54
Deepanshu Saini <sainids785@gmail.com>
2022-11-21 17:51:12 +0530

    added method to extract meals from dummy data based on category id

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // final String categoryId;
@@ -13,10 +14,16 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
-      body: Center(
-        child: Text('The recipe for the category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
