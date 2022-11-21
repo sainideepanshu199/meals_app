9cf0287eae31d81e789d103b580b2969b78e6644
Deepanshu Saini <sainids785@gmail.com>
2022-11-21 17:57:17 +0530

    added sub-folders to manage screens and widgets seperately

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  final String id;
+
+  CategoryItem(this.title, this.color, this.id);
+  void selectCategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .pushNamed('/category-Meals', arguments: {'id': id, 'title': title});
+  }
+
+// constructor is added to assign values to these properties
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              color.withOpacity(0.6),
+              color,
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
+      ),
+    );
+  }
+}
