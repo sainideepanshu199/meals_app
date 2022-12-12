71b4a245ecab4f28b62099eed8b0fca66370c916
Deepanshu Saini <sainids785@gmail.com>
2022-12-12 19:13:16 +0530

    added delete button to remove particular meal

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -33,52 +33,59 @@ class MealDetailScreen extends StatelessWidget {
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${selectedMeal.title}'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 200,
-                width: double.infinity,
-                child: Image.network(
-                  selectedMeal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
               ),
-              buildSectionTitle(context, 'Ingredients'),
-              buildContainer(ListView.builder(
-                itemBuilder: ((context, index) => Card(
-                      // ignore: deprecated_member_use
-                      color: Theme.of(context).accentColor,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 10),
-                        child: Text(selectedMeal.ingredients[index]),
-                      ),
-                    )),
-                itemCount: selectedMeal.ingredients.length,
-              )),
-              buildSectionTitle(context, 'Steps'),
-              buildContainer(ListView.builder(
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        child: Text('# ${(index + 1)}'),
-                      ),
-                      title: Text(
-                        selectedMeal.steps[index],
-                      ),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(ListView.builder(
+              itemBuilder: ((context, index) => Card(
+                    // ignore: deprecated_member_use
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 10),
+                      child: Text(selectedMeal.ingredients[index]),
                     ),
-                    Divider()
-                  ],
-                ),
-                itemCount: selectedMeal.steps.length,
-              ))
-            ],
-          ),
-        ));
+                  )),
+              itemCount: selectedMeal.ingredients.length,
+            )),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(ListView.builder(
+              itemBuilder: (context, index) => Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      child: Text('# ${(index + 1)}'),
+                    ),
+                    title: Text(
+                      selectedMeal.steps[index],
+                    ),
+                  ),
+                  Divider()
+                ],
+              ),
+              itemCount: selectedMeal.steps.length,
+            ))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: (() {
+          Navigator.of(context).pop(mealId);
+        }),
+      ),
+    );
   }
 }
